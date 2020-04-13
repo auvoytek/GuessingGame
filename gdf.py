d37024be21b28191e78873e87c6ad49efa6c3dfe
August Voytek <voytek1017@gmail.com>
2020-04-12 22:12:21 -0500

    remade the program with nested if statements. Now has the funcationality to test if the userInput is larger/smaller than the random generated number. Will create later using a function/class so that code does not need to be in every if statement.

--- gdf.py
+++ gdf.py
@@ -12,30 +12,55 @@ CODE LOGIC
 We need the program to generate a random number and then have the user guess it. 
 The program will need to check if that guess is equal to the random number and if it is within the 0 to 20 range. 
 Program will also need to ensure that the input is a integer and not a string. 
+Need program to send data to a function and then return function value to determine if the attempts have added to 3.
+Using a function to ensure I do not have to retype lines of code. 
 
 """
 
+incorrectGuess = 0
 randomNumber = random.randrange(0, 20)
 gameRun = True
-incorrectGuess = 0
-print("Please input a random number between 0 and 20. You only have 3 tries so make it count.")
 
+print("Please input a random number between 0 and 20. You only have 3 tries so make it count.")
+print(randomNumber)
 
 while gameRun:
-    userInput = input(">")
-    if int(userInput) <= 20:
-        if int(userInput) >= 0:
+    if incorrectGuess == 3:
+        print("Game over you have reached 3 tries")
+        break
+    elif incorrectGuess < 3:
+        userInput = input(">")
+        if (int(userInput) <= 20) and (int(userInput) >= 0):
             if int(userInput) == randomNumber:
                 print("Congrats, you guessed right. Game over!")
                 break
-            else:
-                print("Try again. That was incorrect.")
-                incorrectGuess = incorrectGuess + 1
-    else:
-        print("That number is not between the range. Enter a number between 0 and 20.")
-        incorrectGuess = incorrectGuess + 1
+            elif int(userInput) != randomNumber:
+                tooLow = randomNumber > int(userInput)
+                tooHigh = randomNumber < int(userInput)
+                if tooHigh:
+                    incorrectGuess += 1
+                    if incorrectGuess >= 3:
+                        print("Game over you have reached 3 tries")
+                        break
+                    elif incorrectGuess < 3:
+                        print("You guess was too high")
 
-    if incorrectGuess == 3:
-        print("The game is over.")
-        print("The random number was "+str(randomNumber)+".")
-        break
+                if tooLow:
+                    incorrectGuess += 1
+                    if incorrectGuess >= 3:
+                        print("Game over you have reached 3 tries")
+                        break
+                    elif incorrectGuess < 3:
+                        print("You guess was too low")
+
+        elif int(userInput) > 20:
+            incorrectGuess += 1
+            if incorrectGuess == 3:
+                print("Game over you have reached 3 tries")
+                break
+            elif incorrectGuess < 3:
+                print("Enter a number between 0 and 20.")
+
+
+
+
